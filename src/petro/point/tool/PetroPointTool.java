cc7efed4db4e8526bf17f56d48881af94bb66892
Sathsara <chalithsathsara@gmail.com>
2024-12-08 12:25:03 +0530

    hello boy

--- src/petro/point/tool/PetroPointTool.java
+++ src/petro/point/tool/PetroPointTool.java
@@ -8,6 +8,7 @@ public class PetroPointTool {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello !");
+        System.out.println("Hello boy!");
     }
     
 }
