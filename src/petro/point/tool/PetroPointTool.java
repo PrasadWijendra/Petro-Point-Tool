6188df8092c0884135a6d56918893d34ce4669d2
Adithya Rosayru <arosayru@gmail.com>
2024-12-08 12:17:59 +0530

    Hello Text

--- src/petro/point/tool/PetroPointTool.java
+++ src/petro/point/tool/PetroPointTool.java
@@ -7,7 +7,7 @@ public class PetroPointTool {
  
     public static void main(String[] args) {
         System.out.println("Hello world");
-      
+        System.out.println("Hello !");
     }
     
 }
