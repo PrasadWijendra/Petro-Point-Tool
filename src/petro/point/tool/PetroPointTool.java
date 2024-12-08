33f1d3b804804cf7bdc506afb5353a1ad7cb82aa
Tharanga Prasad <tharangaprasad98@gmail.com>
2024-12-08 12:28:00 +0530

    remove text and update

--- src/petro/point/tool/PetroPointTool.java
+++ src/petro/point/tool/PetroPointTool.java
@@ -7,8 +7,9 @@ public class PetroPointTool {
  
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello !");
-        System.out.println("Hello boy!");
+        System.out.println("Hello gays !");
+        
+        
     }
     
 }
